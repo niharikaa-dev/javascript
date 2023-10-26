6f9ba57eaadd149625173b92e9171bb60d331c35
Niharika <143190628+niharikaa-dev@users.noreply.github.com>
2023-10-26 17:38:19 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,45 @@
+const month = 1
+
+switch (month) {
+    case 1:
+        console.log("jan");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("mar");
+        break;
+    case 4:
+        console.log("april");
+        break;
+    case 5:
+        console.log("may");
+        break;
+    case 6:
+        console.log("june");
+        break;
+    case 7:
+        console.log("july");
+        break;
+    case 8:
+        console.log("aug");
+        break;
+    case 9:
+        console.log("sept");
+        break;
+    case 10:
+        console.log("oct");
+        break;
+    case 11:
+        console.log("nov");
+        break;
+    case 12:
+        console.log("dec");
+        break;
+    
+
+    default:
+        console.log("default match");
+        break;
+}
